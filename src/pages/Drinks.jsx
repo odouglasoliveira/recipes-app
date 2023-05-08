0b237e03ea3e5f8dcf448872b2fa7d383324d052
Douglas Oliveira <doglasolivera13@gmail.com>
2023-05-08 11:05:36 -0300

    feat: criação dos cards de receita para os 12 primeiros itens da lista | requisito 14

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,10 +1,23 @@
+import { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { RecipesContext } from '../context/RecipesContext';
+import { DrinkRecipeCard } from '../components/DrinkRecipeCard';
 
 export default function Drinks() {
+  const { recipes } = useContext(RecipesContext);
   return (
     <>
       <Header pageTitle="Drinks" showSearchIcon />
+      {
+        recipes.map((recipe, index) => (
+          <DrinkRecipeCard
+            key={ recipe.idDrink }
+            index={ index }
+            recipe={ recipe }
+          />
+        ))
+      }
       <Footer />
     </>
   );
