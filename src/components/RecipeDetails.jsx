aac9efc369ec4d4f8520626ee9a800434f4235eb
euHec <hectorsouzass@gmail.com>
2023-05-08 22:21:39 -0300

    Feat: Implementa página de detalhe dos produtos

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -7,19 +7,19 @@ export default function RecipeDetails() {
 
   const fetchAPI = useCallback(async () => {
     const id = location.pathname.split('/').pop();
-    if (location.pathname.includes('/meals')) {
+    if (location.pathname.includes('meals')) {
       const URL_MEALS = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
       const apiMeals = await fetch(URL_MEALS);
       const data = await apiMeals.json();
       console.log(data.meals);
       setItem(data.meals);
     }
-    if (location.pathname.includes('/drinks')) {
+    if (location.pathname.includes('drinks')) {
       const URL_DRINKS = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
       const apiDrinks = await fetch(URL_DRINKS);
       const data = await apiDrinks.json();
-      console.log(data.drink);
-      setItem(data.drink);
+      console.log(data.drinks);
+      setItem(data.drinks);
     }
   }, [location]);
 
@@ -27,30 +27,48 @@ export default function RecipeDetails() {
     fetchAPI();
   }, [fetchAPI]);
 
-  const videoId = item[0]?.strYoutube.split('=').pop();
-
   return (
     <>
       <img
         data-testid="recipe-photo"
-        src={ item[0]?.strMealThumb}
-        alt={ item[0]?.strMeal }
+        src={
+          location.pathname.includes('/meals')
+          ? item[0]?.strMealThumb
+          : item[0]?.strDrinkThumb
+        }
+        alt={
+          location.pathname.includes('/meals')
+          ? item[0]?.strMeal
+          : item[0]?.strDrinkThumb
+        }
         width="325"
         height="200"
       />
 
-      <h1 data-testid="recipe-title">{ item[0]?.strMeal }</h1>
+      <h1 data-testid="recipe-title">
+        {
+          location.pathname.includes('/meals')
+          ? item[0]?.strMeal
+          : item[0]?.strDrink
+        }
+      </h1>
 
-      <h1 data-testid="recipe-category">{ item[0]?.strCategory}</h1>
+      <h1 data-testid="recipe-category">
+        {
+          location.pathname.includes('/meals')
+          ? item[0]?.strCategory
+          : item[0]?.strAlcoholic
+        }
+      </h1>
 
       {
         item.length !== 0 && (
           Object.keys(item[0]).map((key, index) => {
-            if (key.includes('strIngredient')) {
+            if (key.includes('strIngredient') && item[0][key] !== null) {
               const ingredientIndex = parseInt(key.substring(13)) - 1;
               return (
                 <p key={index} data-testid={`${ingredientIndex}-ingredient-name-and-measure`}>
-                  {item[0][key]}
+                  { `${item[0][key]} ${item[0][`strMeasure${ingredientIndex + 1}`]}` }
                 </p>
               );
             }
@@ -64,9 +82,10 @@ export default function RecipeDetails() {
       {
         location.pathname.includes('meals') && (
           <iframe
+            data-testid="video"
             width="325"
             height="200"
-            src={ `https://www.youtube.com/embed/${videoId}` }
+            src={ `https://www.youtube.com/embed/${item[0]?.strYoutube.split('=').pop()}` }
             title="Embedded YouTube video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
