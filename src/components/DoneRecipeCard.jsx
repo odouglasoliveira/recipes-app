c3b3dfd944b401757a657a01d788ebc3272346da
Luanderson Alves <luaoderson@gmail.com>
2023-05-11 00:24:11 -0300

    feat: botão de compartilhar receita funcionando

--- src/components/DoneRecipeCard.jsx
+++ src/components/DoneRecipeCard.jsx
@@ -1,10 +1,33 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import shareIconSvg from '../images/shareIcon.svg';
 
+const MIL = 1000;
+
 export default function DoneRecipeCard({ recipe, index }) {
+  const [isCopied, setIsCopied] = useState(false);
+  const [timeIsCopied, setTimeIsCopied] = useState(1);
+  const [idTimeIsCopied, setIdTimeIsCopied] = useState(0);
+
   const {
-    image, category, nationality, name, doneDate, tags, alcoholicOrNot,
+    image, category, nationality, name, doneDate, tags, alcoholicOrNot, id, type,
   } = recipe;
+
+  const handleClickCopy = () => {
+    const { location: { protocol, host } } = window;
+    navigator.clipboard.writeText(`${protocol}//${host}/${type}s/${id}`);
+    setIsCopied(true);
+    setIdTimeIsCopied(setInterval(() => setTimeIsCopied((prev) => prev - 1), MIL));
+  };
+
+  useEffect(() => {
+    if (!timeIsCopied) {
+      clearInterval(idTimeIsCopied);
+      setIsCopied(false);
+      setTimeIsCopied(1);
+    }
+  }, [idTimeIsCopied, timeIsCopied]);
+
   return (
     <li>
       <img
@@ -28,9 +51,16 @@ export default function DoneRecipeCard({ recipe, index }) {
           </ul>
         ))
       }
-      <button src={ shareIconSvg } data-testid={ `${index}-horizontal-share-btn` }>
+      <button
+        src={ shareIconSvg }
+        data-testid={ `${index}-horizontal-share-btn` }
+        onClick={ handleClickCopy }
+      >
         <img src={ shareIconSvg } alt="shareIcon" />
       </button>
+      {
+        isCopied && <span>Link copied!</span>
+      }
     </li>
   );
 }
@@ -43,6 +73,8 @@ DoneRecipeCard.propTypes = {
     doneDate: PropTypes.string,
     nationality: PropTypes.string,
     alcoholicOrNot: PropTypes.string,
+    type: PropTypes.string,
+    id: PropTypes.number,
     tags: PropTypes.arrayOf(PropTypes.string),
   }).isRequired,
   index: PropTypes.number.isRequired,
