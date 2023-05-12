2afb5a8803a25dd545e87eed742f92d073e33da7
Douglas Oliveira <doglasolivera13@gmail.com>
2023-05-12 17:47:56 -0300

    feat: criação dos botões da tela Profile | requisitos 57, 58 e 59

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,9 +2,14 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
     <>
       <Header pageTitle="Profile" />
+      <p data-testid="profile-email">{user.email}</p>
+      <button data-testid="profile-done-btn">Done Recipes</button>
+      <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn">Logout</button>
       <Footer />
     </>
   );
